67dd61d94e2357e8e95d504c186e5aa32fd5c92c
Madeline Dale <mhosanna@gmail.com>
2021-12-14 07:45:29 -0500

    main package and main function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mhosanna/code-challenges
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	merrychristmas "github.com/mhosanna/code-challenges/2021-12-13"
+)
+
+func main() {
+	merrychristmas.MakeTree()
+}
